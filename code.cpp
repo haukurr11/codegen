59e92abee10c34f48165f4686876b0d6610624de
haukurr11 <haukurr11@ru.is>
2013-11-09 03:47:30 +0000

    code.cpp almost ready

--- code.cpp
+++ code.cpp
@@ -49,35 +49,48 @@ Code::~Code()
 
 void Code::generate(CodeOp op, SymbolTableEntry* arg1, SymbolTableEntry* arg2, SymbolTableEntry* result)
 {
-
+  Quadruple quad(op,arg1,arg2,result);
+  m_qList.push_back(quad);
 }
 
 void Code::generateCall(SymbolTableEntry* entry, EntryList& eList)
 {
-
+  EntryList::iterator it;
+  for(it = eList.begin(); it != eList.end(); ++it)
+    generate(cd_APARAM,NULL,NULL,*it);
+  generate(cd_CALL,entry,NULL,NULL);
 }
 
 void Code::generateFormals(EntryList& entrylist)
 {
-
+  EntryList::iterator it;
+  for(it = entrylist.begin(); it != entrylist.end(); ++it)
+    generate(cd_FPARAM,NULL,NULL,*it);
 }
 
 void Code::generateVariables(EntryList& entrylist)
 {
-
+  EntryList::iterator it;
+  for(it = entrylist.begin(); it != entrylist.end(); ++it)
+    generate(cd_VAR,NULL,NULL,*it);
 }
 
 std::string Code::newLabel()
 {
-
+  m_labelCounter++;
+  return "L" + m_labelCounter;
 }
 
 std::string Code::newTemp()
 {
-
+  m_tempCounter++;
+  return "t" + m_tempCounter;
 }
 
 void Code::print()
 {
-
+  QuadrupleList::iterator it;
+  for(it = m_qList.begin(); it != m_qList.end(); ++it)
+    it->print(*it);
 }
+
