14841750c76f2d1d278d692db232a99429bbdaa1
haukurr11 <haukurr11@ru.is>
2013-11-09 02:02:12 +0000

    Initialize your symbol table with the constants (lexemes) “0” and “1”

--- symtab.cpp
+++ symtab.cpp
@@ -1,42 +1,44 @@
 #include "symtab.h"
+#include "code.h"
 
 SymbolTable::SymbolTable(void)
 {
-	
+  insert(CodeFalse);
+  insert(CodeTrue);
 }
+
 SymbolTable::~SymbolTable(void)
 {
-	
-	for(std::size_t n = 0 ; n < m_table.size() ; n++)
-	{
-		delete m_table[n]; 
-	}
+  for(std::size_t n = 0 ; n < m_table.size() ; n++)
+  {
+    delete m_table[n]; 
+  }
 }
 
 SymbolTableEntry* SymbolTable::insert(std::string lexeme)
 {
-	std::transform(lexeme.begin(), lexeme.end(), lexeme.begin(), ::tolower);
-	SymbolTableEntry* entry = lookup(lexeme);
-	if(entry)
-		return entry;
+  std::transform(lexeme.begin(), lexeme.end(), lexeme.begin(), ::tolower);
+  SymbolTableEntry* entry = lookup(lexeme);
+  if(entry)
+    return entry;
 
-	// Not found, create
-	entry = new SymbolTableEntry();
-	entry->lexeme = lexeme;
-	m_table.push_back(entry);
-	return entry;
+  // Not found, create
+  entry = new SymbolTableEntry();
+  entry->lexeme = lexeme;
+  m_table.push_back(entry);
+  return entry;
 }
 SymbolTableEntry* SymbolTable::lookup(std::string lexeme)
 {
-	std::transform(lexeme.begin(), lexeme.end(), lexeme.begin(), ::tolower);
-	SymbolTableEntry* entry = NULL;
-	for(std::size_t n = 0 ; n < m_table.size() ; n++)
-	{
-		if(m_table[n]->lexeme == lexeme)
-		{
-			entry = m_table[n];
-			break;
-		}
-	}
-	return entry;
+  std::transform(lexeme.begin(), lexeme.end(), lexeme.begin(), ::tolower);
+  SymbolTableEntry* entry = NULL;
+  for(std::size_t n = 0 ; n < m_table.size() ; n++)
+  {
+    if(m_table[n]->lexeme == lexeme)
+    {
+      entry = m_table[n];
+      break;
+    }
+  }
+  return entry;
 }
